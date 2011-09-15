e51248d9bcbfcf74e701ffdeb44bafb48e07d57e
Rowan Crawford <wombleton@gmail.com>
2011-09-15 18:37:11 +1200

    Add in REPL for managing cluster

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 cs = require('coffee-script')
 cluster = require('cluster')
 
-cluster('app').listen(80)
+cluster('app')
+  .use(cluster.repl('/var/run/cluster.sock'))
+  .listen(80)
 //require('./app').listen(3000)
