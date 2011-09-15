b7f48e48b84540ec65c5b43b8b3b9a48844d6775
Rowan Crawford <wombleton@gmail.com>
2011-09-15 18:48:15 +1200

    Add production run shell script

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 cs = require('coffee-script')
 cluster = require('cluster')
 
-cluster('app')
-  .use(cluster.repl('/var/run/cluster.sock'))
-  .listen(80)
+cluster('app').listen(80)
 //require('./app').listen(3000)
